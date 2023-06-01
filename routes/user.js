00b26766d9b4c20863a395725651af399d3ff4ce
Aleksandr Sitnikov <gnomek100500@gmail.com>
2023-06-01 04:46:03 +0300

    featured: Сделать запросы к users и cards

---
+++ routes/user.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+const { getUsers, createUsers, getUserById, updateProfile, updateAvatar } = require('../controllers/users');
+
+router.get('/users', getUsers);
+router.get('/users/:userId', getUserById);
+router.post('/users', createUsers);
+router.patch('/users/me', updateProfile);
+router.patch('/users/me/avatar', updateAvatar);
+module.exports = router;
