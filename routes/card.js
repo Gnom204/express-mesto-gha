b0e3ca62075430d3b2452a7f037499d5e6047539
Aleksandr Sitnikov <gnomek100500@gmail.com>
2023-06-20 20:47:57 +0300

    hotfix: изменить валидацию

--- routes/card.js
+++ routes/card.js
@@ -3,11 +3,10 @@ const cardRouter = require('express').Router();
 const {
   getCards, createCards, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
-const { createCardValidation, cardIdValidation } = require('../utils/joiValidation');
 
 cardRouter.get('/', getCards);
-cardRouter.post('/', createCardValidation, createCards);
-cardRouter.delete('/:cardId', cardIdValidation, deleteCard);
-cardRouter.put('/:cardId/likes', cardIdValidation, likeCard);
-cardRouter.delete('/:cardId/likes', cardIdValidation, dislikeCard);
+cardRouter.post('/', createCards);
+cardRouter.delete('/:cardId', deleteCard);
+cardRouter.put('/:cardId/likes', likeCard);
+cardRouter.delete('/:cardId/likes', dislikeCard);
 module.exports = cardRouter;
--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,10 @@ const userRouter = require('express').Router();
 const {
   getUsers, getUserById, updateProfile, updateAvatar, getUserInfo,
 } = require('../controllers/users');
-const { updateProfileValidation, updateAvatarValidation, userIdValidation } = require('../utils/joiValidation');
 
 userRouter.get('/', getUsers);
-userRouter.get('/:userId', userIdValidation, getUserById);
+userRouter.get('/:userId', getUserById);
 userRouter.get('/me', getUserInfo);
-userRouter.patch('/me', updateProfileValidation, updateProfile);
-userRouter.patch('/me/avatar', updateAvatarValidation, updateAvatar);
+userRouter.patch('/me', updateProfile);
+userRouter.patch('/me/avatar', updateAvatar);
 module.exports = userRouter;
