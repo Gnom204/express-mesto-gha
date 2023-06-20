026cb26a7be9fd7ee4c19009172f071f980ff5dc
Aleksandr Sitnikov <gnomek100500@gmail.com>
2023-06-20 20:49:01 +0300

    hotfix: Огузок не справился с Joi

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,11 @@ const userRouter = require('./user');
 const cardRouter = require('./card');
 const { login, createUsers } = require('../controllers/users');
 const defenseRouter = require('../middlewares/auth');
-const { loginValidation, createUserValidation } = require('../utils/joiValidation');
 
 const router = express.Router();
 
-router.post('/signin', loginValidation, login);
-router.post('/signup', createUserValidation, createUsers);
+router.post('/signin', login);
+router.post('/signup', createUsers);
 
 router.use(defenseRouter);
 router.use('/users', userRouter);
